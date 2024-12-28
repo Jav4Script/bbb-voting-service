efee84947ec304fd41d9326674b1f6c094c94c54
Anderson Barros <abarros@ciandt.com>
2024-12-28 18:16:44 -0300

    feat: update models

--- internal/infrastructure/models/participant_model.go
+++ internal/infrastructure/models/participant_model.go
@@ -1,15 +1,20 @@
 package models
 
 import (
+	"time"
+
 	entities "bbb-voting-service/internal/domain/entities"
 
 	"github.com/google/uuid"
 )
 
 type ParticipantModel struct {
-	ID    uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	Name  string    `gorm:"not null"`
-	Votes int       `gorm:"not null"`
+	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Name      string    `gorm:"not null"`
+	Age       int       `gorm:"not null"`
+	Gender    string    `gorm:"not null"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
 }
 
 func (ParticipantModel) TableName() string {
@@ -18,18 +23,26 @@ func (ParticipantModel) TableName() string {
 
 func ToDomainParticipant(participantModel ParticipantModel) entities.Participant {
 	return entities.Participant{
-		ID:    participantModel.ID.String(),
-		Name:  participantModel.Name,
-		Votes: participantModel.Votes,
+		ID:        participantModel.ID.String(),
+		Name:      participantModel.Name,
+		Age:       participantModel.Age,
+		Gender:    participantModel.Gender,
+		CreatedAt: participantModel.CreatedAt.Format(time.RFC3339),
+		UpdatedAt: participantModel.UpdatedAt.Format(time.RFC3339),
 	}
 }
 
 func ToModelParticipant(participant entities.Participant) ParticipantModel {
 	id, _ := uuid.Parse(participant.ID)
+	createdAt, _ := time.Parse(time.RFC3339, participant.CreatedAt)
+	updatedAt, _ := time.Parse(time.RFC3339, participant.UpdatedAt)
 
 	return ParticipantModel{
-		ID:    id,
-		Name:  participant.Name,
-		Votes: participant.Votes,
+		ID:        id,
+		Name:      participant.Name,
+		Age:       participant.Age,
+		Gender:    participant.Gender,
+		CreatedAt: createdAt,
+		UpdatedAt: updatedAt,
 	}
 }
--- internal/infrastructure/models/vote_model.go
+++ internal/infrastructure/models/vote_model.go
@@ -9,15 +9,15 @@ import (
 )
 
 type VoteModel struct {
-	ID                 uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	ParticipantID      string    `gorm:"not null"`
-	VoterID            string    `gorm:"not null"`
-	Timestamp          time.Time `gorm:"not null"`
-	IPAddress          string    `gorm:"not null"`
-	UserAgent          string    `gorm:"not null"`
-	Region             string    `gorm:"not null"`
-	Device             string    `gorm:"not null"`
-	IsCaptchaCompleted bool      `gorm:"not null"`
+	ID            uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	ParticipantID string    `gorm:"not null"`
+	VoterID       string    `gorm:"not null"`
+	IPAddress     string    `gorm:"not null"`
+	UserAgent     string    `gorm:"not null"`
+	Region        string    `gorm:"not null"`
+	Device        string    `gorm:"not null"`
+	CreatedAt     time.Time `gorm:"autoCreateTime"`
+	UpdatedAt     time.Time `gorm:"autoUpdateTime"`
 }
 
 func (VoteModel) TableName() string {
@@ -26,30 +26,32 @@ func (VoteModel) TableName() string {
 
 func ToDomainVote(voteModel VoteModel) entities.Vote {
 	return entities.Vote{
-		ID:                 voteModel.ID.String(),
-		ParticipantID:      voteModel.ParticipantID,
-		VoterID:            voteModel.VoterID,
-		Timestamp:          voteModel.Timestamp,
-		IPAddress:          voteModel.IPAddress,
-		UserAgent:          voteModel.UserAgent,
-		Region:             voteModel.Region,
-		Device:             voteModel.Device,
-		IsCaptchaCompleted: voteModel.IsCaptchaCompleted,
+		ID:            voteModel.ID.String(),
+		ParticipantID: voteModel.ParticipantID,
+		VoterID:       voteModel.VoterID,
+		IPAddress:     voteModel.IPAddress,
+		UserAgent:     voteModel.UserAgent,
+		Region:        voteModel.Region,
+		Device:        voteModel.Device,
+		CreatedAt:     voteModel.CreatedAt.Format(time.RFC3339),
+		UpdatedAt:     voteModel.UpdatedAt.Format(time.RFC3339),
 	}
 }
 
 func ToModelVote(vote entities.Vote) VoteModel {
 	id, _ := uuid.Parse(vote.ID)
+	createdAt, _ := time.Parse(time.RFC3339, vote.CreatedAt)
+	updatedAt, _ := time.Parse(time.RFC3339, vote.UpdatedAt)
 
 	return VoteModel{
-		ID:                 id,
-		ParticipantID:      vote.ParticipantID,
-		VoterID:            vote.VoterID,
-		Timestamp:          vote.Timestamp,
-		IPAddress:          vote.IPAddress,
-		UserAgent:          vote.UserAgent,
-		Region:             vote.Region,
-		Device:             vote.Device,
-		IsCaptchaCompleted: vote.IsCaptchaCompleted,
+		ID:            id,
+		ParticipantID: vote.ParticipantID,
+		VoterID:       vote.VoterID,
+		IPAddress:     vote.IPAddress,
+		UserAgent:     vote.UserAgent,
+		Region:        vote.Region,
+		Device:        vote.Device,
+		CreatedAt:     createdAt,
+		UpdatedAt:     updatedAt,
 	}
 }
